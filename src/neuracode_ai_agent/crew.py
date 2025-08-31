5e9c94cc4727af25c6336b0c03ea810e53d93e80
Sudeepa-R <sudeepar084@gmail.com>
2025-08-31 19:47:49 +0530

    module notfound error fix

--- src/neuracode_ai_agent/crew.py
+++ src/neuracode_ai_agent/crew.py
@@ -3,7 +3,7 @@ from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
 import os
-# If you want to run a snippet of code before or after the crew starts,
+# If you want to run a snippet of code before or after the crew starts, 
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
 
